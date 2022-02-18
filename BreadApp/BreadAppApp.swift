6e609b7b92f77c19da1f78ff3d7cdd0d8b4fab4a
Sam Hertz <samhertz@vt.edu>
2022-02-17 19:35:59 -0800

    Initial Commit

---
+++ BreadApp/BreadAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreadAppApp.swift
+//  BreadApp
+//
+//  Created by Samuel Hertz on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct BreadAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreadApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BreadApp
+//
+//  Created by Samuel Hertz on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
